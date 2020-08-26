0b849dba5d5aaafd425910aa6396027da27473c4
Ngoc Anh <ngocanhng6297@gmail.com>
2020-08-26 12:47:32 +0700

    deploy

--- index.js
+++ index.js
@@ -50,11 +50,14 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "client")));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "client")));
+}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 
 server.listen(PORT, (err) => {
   console.log(err || `Server opend at port '${PORT}'`);
