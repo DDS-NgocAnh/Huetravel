652540df94d8b2895f7f7ded4a0c7a9dbe2378b6
Ngoc Anh <ngocanhng6297@gmail.com>
2020-08-27 13:41:33 +0700

    fix: not confirm email

--- connect-mongo.js
+++ connect-mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const connectionString = process.env.MONGO_URL 
-// const connectionString = 'mongodb://localhost:27017/test'
+// const connectionString = process.env.MONGO_URL 
+const connectionString = 'mongodb://localhost:27017/test'
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
